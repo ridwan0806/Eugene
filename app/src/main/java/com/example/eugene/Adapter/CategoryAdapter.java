7f8aba5c4926b16f478ca414cbfcbdd46765de81
ridwan0806 <ridwanm32@gmail.com>
2023-04-22 17:52:05 +0700

    add click listener on category

--- app/src/main/java/com/example/eugene/Adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/eugene/Adapter/CategoryAdapter.java
@@ -6,12 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.eugene.Interface.ItemClickListener;
 import com.example.eugene.Model.Category;
 import com.example.eugene.R;
 
@@ -40,6 +42,13 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         holder.categoryName.setText(category.getName());
         Glide.with(holder.itemView.getContext()).load(list.get(position).getImage()).into(holder.categoryPic);
 
+        holder.setItemClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                Toast.makeText(context, category.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
@@ -47,16 +56,29 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         return list.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView categoryName;
         ImageView categoryPic;
         ConstraintLayout mainLayout;
+
+        private ItemClickListener itemClickListener;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             categoryName = itemView.findViewById(R.id.categoryName);
             categoryPic = itemView.findViewById(R.id.categoryPic);
             mainLayout = itemView.findViewById(R.id.mainLayout);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void setItemClickListener(ItemClickListener itemClickListener) {
+            this.itemClickListener = itemClickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            itemClickListener.onClick(view,getBindingAdapterPosition(),false);
         }
     }
 }
