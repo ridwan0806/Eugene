e83802b1e225c45771eef4ab4eec54040abd805b
ridwan0806 <ridwanm32@gmail.com>
2023-05-18 09:40:42 +0700

    20230518 add layout master food

--- app/src/main/java/com/example/eugene/DashboardActivity.java
+++ app/src/main/java/com/example/eugene/DashboardActivity.java
@@ -2,13 +2,53 @@ package com.example.eugene;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity {
+    ImageView masterFood,masterDrink,masterRawMaterial,masterExpenses;
+    ImageView cashier,addOrder,addPurchaseOrder,addRoutineExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        initView();
+        initIntent();
+    }
+
+    private void initIntent() {
+        masterFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent masterFood = new Intent(DashboardActivity.this,MasterFoods.class);
+                startActivity(masterFood);
+//                Toast.makeText(DashboardActivity.this, "ini master food", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        masterDrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashboardActivity.this, "ini master drink", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        masterRawMaterial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(DashboardActivity.this, "ini master RawMaterial", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initView() {
+        masterFood = findViewById(R.id.imgDashboardFood);
+        masterDrink = findViewById(R.id.imgDashboardDrink);
+        masterRawMaterial = findViewById(R.id.imgDashboardRaw);
     }
 }
---
+++ app/src/main/java/com/example/eugene/MasterFoods.java
@@ -0,0 +1,14 @@
+package com.example.eugene;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MasterFoods extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master_foods);
+    }
+}
