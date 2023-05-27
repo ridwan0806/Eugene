8c99b2391052ffa96b740a00368f7e95a8366daf
ridwan0806 <ridwanm32@gmail.com>
2023-05-27 20:34:39 +0700

    20230527 revisi layout order info

--- app/src/main/java/com/example/eugene/Activity/Cart.java
+++ app/src/main/java/com/example/eugene/Activity/Cart.java
@@ -2,21 +2,27 @@ package com.example.eugene.Activity;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.eugene.Adapter.CartAdapter;
 import com.example.eugene.DashboardActivity;
 import com.example.eugene.Database.DatabaseHelper;
+import com.example.eugene.MasterFoods;
 import com.example.eugene.Model.Orders;
 import com.example.eugene.Model.Request;
 import com.example.eugene.R;
@@ -37,7 +43,11 @@ public class Cart extends AppCompatActivity {
 
     CartAdapter cartAdapter;
 
-    TextView txtTotalAll,btnSubmitOrder;
+    TextView txtTotalAll,btnSubmitOrder,consumenType,notes;
+    EditText customerName;
+    RadioGroup orderType;
+    RadioButton rdDineIn,rdTakeAway;
+    int categoryTypeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +68,7 @@ public class Cart extends AppCompatActivity {
         btnSubmitOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showAlertDialog();
+                submitShowDialog();
             }
         });
 
@@ -69,6 +79,73 @@ public class Cart extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         loadListFood();
+    }
+
+    private void submitShowDialog(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Cart.this);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View submitLayout = inflater.inflate(R.layout.cart_add_detail_info,null);
+
+        alertDialog.setView(submitLayout);
+
+        customerName = submitLayout.findViewById(R.id.consumenName);
+        orderType = submitLayout.findViewById(R.id.rdOrderType);
+        rdDineIn = submitLayout.findViewById(R.id.rdDineIn);
+        rdTakeAway = submitLayout.findViewById(R.id.rdTakeAway);
+        consumenType = submitLayout.findViewById(R.id.cmb_consumenType);
+        notes = submitLayout.findViewById(R.id.edtNotes);
+
+        consumenType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                categoryTypeId = 0;
+                PopupMenu popup = new PopupMenu(Cart.this, consumenType);
+                popup.getMenuInflater().inflate(R.menu.category_consumen, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        int itemId = item.getItemId();
+                        if (itemId == R.id.umum) {
+                            categoryTypeId = 1;
+                            consumenType.setText("Umum");
+                        } else if (itemId == R.id.online){
+                            categoryTypeId = 2;
+                            consumenType.setText("Online / Ojol");
+                        } else if (itemId == R.id.karyawan){
+                            categoryTypeId = 3;
+                            consumenType.setText("Karyawan");
+                        } else if (itemId == R.id.kerabat){
+                            categoryTypeId = 4;
+                            consumenType.setText("Kerabat / Tetangga");
+                        } else {
+                            categoryTypeId = 0;
+                        }
+                        System.out.println(categoryTypeId);
+                        return true;
+                    }
+                });
+                popup.show();
+            }
+        });
+
+        //SUBMIT ORDER TO FIREBASE
+        alertDialog.setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Cart.this, "Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alertDialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Cart.this, "Canceled", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alertDialog.show();
     }
 
     private void showAlertDialog() {
